97c758b724c69aad9bfcecc7ed4ccf272f274841
Nathan <167822258+danielanonym90@users.noreply.github.com>
2025-06-15 16:56:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,70 +9,91 @@ class Notes {
         this._tarefas = {}
     }
 
-    _criarNotaDOM (titulo, descricao) {
-          //Comeca aqui 
-            const actionDiv = document.createElement('div')
-            actionDiv.classList.add('actions')
+    _criarNotaDOM(titulo, descricao, concluido = false) {
 
-            const newButton = document.createElement('button')
-            newButton.classList.add('buttonDelete')
-            newButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash" viewBox="0 0 16 16">
+        const textBack = document.createElement('h3')
+        const descriptionBack = document.createElement('p')
+        textBack.classList.add('texto')
+        descriptionBack.classList.add('descricao')
+
+        textBack.innerText = titulo
+        descriptionBack.innerText = descricao
+
+
+
+        //Comeca aqui 
+        const actionDiv = document.createElement('div')
+        actionDiv.classList.add('actions')
+
+        const newButton = document.createElement('button')
+        newButton.classList.add('buttonDelete')
+        newButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
 </svg>`
-            const newDiv = document.createElement('div')
-            newDiv.classList.add('preview')
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('preview')
 
-            newButton.addEventListener('click', (e) => {
+        newButton.addEventListener('click', (e) => {
 
-                const card = e.target.closest('.preview')
-                if (card) card.remove()
-                this.removerTarefa(titulo)
-                newDiv.remove()
-            })
-
-
-
-
-            const checkBox = document.createElement('input')
-            checkBox.type = 'checkbox'
-            checkBox.addEventListener('change', () => {
-                if (checkBox.checked) {
-                    textBack.style.textDecoration = 'line-through'
-                    descriptionBack.style.textDecoration = 'line-through'
-                } else {
-                    textBack.style.textDecoration = 'none'
-                    descriptionBack.style.textDecoration = 'none'
-                }
-            })
-            actionDiv.appendChild(newButton)
-            actionDiv.appendChild(checkBox)
+            const card = e.target.closest('.preview')
+            if (card) card.remove()
+            this.removerTarefa(titulo)
+            newDiv.remove()
+        })
 
 
 
 
-            const textBack = document.createElement('h3')
-            const descriptionBack = document.createElement('p')
-            textBack.classList.add('texto')
-            descriptionBack.classList.add('descricao')
+
+        const checkBox = document.createElement('input')
+        checkBox.type = 'checkbox'
+        checkBox.classList.add('checkbox')
+        checkBox.checked = concluido
+
+        checkBox.checked = concluido
+
+        // Aplica o estilo ao criar a nota com base no valor vindo do localStorage
+        if (concluido) {
+            textBack.style.textDecoration = 'line-through'
+            descriptionBack.style.textDecoration = 'line-through'
+        } else {
+            textBack.style.textDecoration = 'none'
+            descriptionBack.style.textDecoration = 'none'
+        }
 
 
-            textBack.innerText = titulo
-            descriptionBack.innerText = descricao
+        checkBox.addEventListener('change', () => {
+            const isChecked = checkBox.checked
+            this._tarefas[titulo].concluido = isChecked
+            this.salvarNoLocalStorage()
 
-            
+            // Usa isChecked (estado atual do checkbox), não "concluido"
+            if (isChecked) {
+                textBack.style.textDecoration = 'line-through'
+                descriptionBack.style.textDecoration = 'line-through'
+            } else {
+                textBack.style.textDecoration = 'none'
+                descriptionBack.style.textDecoration = 'none'
+            }
+        })
 
 
-            const div1 = document.createElement('div')
-            div1.classList.add('div1')
-            div1.appendChild(textBack)
-            div1.appendChild(descriptionBack)
-
-            newDiv.appendChild(div1)
-            newDiv.appendChild(actionDiv)
+        actionDiv.appendChild(newButton)
+        actionDiv.appendChild(checkBox)
 
 
-            this._main.appendChild(newDiv)
+
+        const div1 = document.createElement('div')
+        div1.classList.add('div1')
+        div1.appendChild(textBack)
+        div1.appendChild(descriptionBack)
+
+        newDiv.appendChild(div1)
+        newDiv.appendChild(actionDiv)
+
+
+        this._main.appendChild(newDiv)
 
 
     }
@@ -96,15 +117,16 @@ class Notes {
         )
         console.log(this._tarefas)
 
-        for (const [titulo, descricao] of Object.entries(this._tarefas)) {
-           
-            this._criarNotaDOM(titulo, descricao)
+        for (const [titulo, dados] of Object.entries(this._tarefas)) {
+
+            this._criarNotaDOM(titulo, dados.descricao, dados.concluido)
 
 
 
 
 
         }
+
     }
 
     adicionarTarefa() {
@@ -114,7 +136,10 @@ class Notes {
 
         if (titulo && descricao) {
 
-            this._tarefas[titulo] = descricao
+            this._tarefas[titulo] = {
+                descricao: descricao,
+                concluido: false
+            }
 
 
             this.salvarNoLocalStorage()
@@ -157,84 +182,16 @@ class Notes {
         this._blurElement.classList.add('esconder');
 
     }
-    criarNota() {
 
-        const actionDiv = document.createElement('div')
-        actionDiv.classList.add('actions')
-
-        const newButton = document.createElement('button')
-        newButton.classList.add('buttonDelete')
-        newButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash" viewBox="0 0 16 16">
-  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-</svg>`
-
-        newButton.addEventListener('click', (e) => {
-
-            const card = e.target.closest('.preview')
-            if (card) card.remove()
-            this.removerTarefa(titulo)
-            newDiv.remove()
-        })
-
-
-
-
-
-        const checkBox = document.createElement('input')
-        checkBox.type = 'checkbox'
-        checkBox.addEventListener('change', () => {
-            if (checkBox.checked) {
-                textBack.style.textDecoration = 'line-through'
-                descriptionBack.style.textDecoration = 'line-through'
-            } else {
-                textBack.style.textDecoration = 'none'
-                descriptionBack.style.textDecoration = 'none'
-            }
-        })
-        actionDiv.appendChild(newButton)
-        actionDiv.appendChild(checkBox)
-
-
-        const newDiv = document.createElement('div')
-        newDiv.classList.add('preview')
-
-        const textBack = document.createElement('h3')
-        const descriptionBack = document.createElement('p')
-        textBack.classList.add('texto')
-        descriptionBack.classList.add('descricao')
-
-        textBack.innerText = this._title.value
-        descriptionBack.innerText = this._description.value
-
-        this._tarefas[textBack.innerText] = descriptionBack.innerText
-        this.salvarNoLocalStorage()
-
-
-        const div1 = document.createElement('div')
-        div1.classList.add('div1')
-        div1.appendChild(textBack)
-        div1.appendChild(descriptionBack)
-
-        newDiv.appendChild(div1)
-        newDiv.appendChild(actionDiv)
-
-
-        this._main.appendChild(newDiv)
-        this._popUp.classList.add('esconder');
-        this._blurElement.classList.add('esconder');
-
-
-
-
-    }
 }
 const notes = new Notes()
 
 notes.onload();
 
 document.getElementById('create').addEventListener('click', () => { notes.mostrarBtn() })
-document.getElementById('createbtn').addEventListener('click', () => { notes.criarNota() })
+document.getElementById('createbtn').addEventListener('click', () => {
+    notes.adicionarTarefa()
+})
 document.getElementById('close').addEventListener('click', () => { notes.fecharBtn() })
 
 
